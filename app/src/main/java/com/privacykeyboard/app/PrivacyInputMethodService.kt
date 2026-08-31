a1bc8c26b93c5a3898eaeeda2b85ba17362d9ff0
Antonysrm Nafi <antonysrmnafi@gmail.com>
2026-08-31 14:58:18 +0600

    Remove touchable insets in PrivacyInputMethodService
    Remove touchable insets setting in onComputeInsets method.

--- app/src/main/java/com/privacykeyboard/app/PrivacyInputMethodService.kt
+++ app/src/main/java/com/privacykeyboard/app/PrivacyInputMethodService.kt
@@ -330,7 +330,6 @@ class PrivacyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardA
         super.onComputeInsets(outInsets)
         outInsets.contentTopInsets = 0
         outInsets.visibleTopInsets = 0
-        outInsets.touchableInsets = InputMethodService.TOUCHABLE_INSET_VISIBLE
     }
 
     // Point 3: some devices render a separate "extract view" (fullscreen editing
